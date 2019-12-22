b219b97bed630fc1caa133b32e3bc547640df1f0
MCJack123 <jackmacwindowslinux@gmail.com>
2019-12-22 04:58:59 -0500

    Fixed non-Mac builds

--- src/lj_alloc.c
+++ src/lj_alloc.c
@@ -176,8 +176,7 @@ static LJ_AINLINE int CALL_MUNMAP(void *ptr, size_t size)
 
 #if LJ_64
 /* 64 bit mode needs special support for allocating memory in the lower 2GB. */
-#undef MAP_32BIT
-#if defined(MAP_32BIT)
+#if defined(MAP_32BIT) && !defined(__APPLE__)
 
 #if defined(__sun__)
 #define MMAP_REGION_START	((uintptr_t)0x1000)
